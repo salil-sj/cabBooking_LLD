827e76dde528151453a5ccdb8b0739f3c6d7e44b
saliljoshi <salilsj1619@gmail.com>
2025-04-15 00:22:50 +0530

    Project Setup

---
+++ src/main/java/com/cab/booking/lld/cab/booking/lld/CabBookingLldApplication.java
@@ -0,0 +1,13 @@
+package com.cab.booking.lld.cab.booking.lld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingLldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cab/booking/lld/cab/booking/lld/CabBookingLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cab.booking.lld.cab.booking.lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookingLldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
